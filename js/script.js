8d51301333e221cbb04edf0fc863a862c5d43483
Patrycja Komenda <patrycjakomenda8@gmail.com>
2022-11-03 08:11:32 +0100

    Add else if

--- js/script.js
+++ js/script.js
@@ -27,11 +27,32 @@ randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
   computerMove = 'kamień';
-} else {
-  computerMove = 'nieznany ruch';
 } else if (randomNumber == '2') {
 	computerMove = 'papier';
 } else if (randomNumber == '3') {
 	computerMove = 'nożyce';
-}
+} else {
+	computerMove = 'nieznany ruch';
+  }
 printMessage('Mój ruch: ' + computerMove);
+
+//add prompt
+var playerInput, playerMove;
+if ( equal ) {
+} else {
+}
+if ( equal ) {
+} else {
+}
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+	playerMove = 'papier';
+} else if (playerInput == '3') {
+	playerMove = 'nożyce';
+} else {
+	playerMove = 'nieznany ruch';
+  }
+printMessage('Twój ruch: ' + playerMove);
