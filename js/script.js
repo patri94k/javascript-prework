0d1398cad54afdf6ca415aaca1c3ef5378bc3619
Patrycja Komenda <patrycjakomenda8@gmail.com>
2022-11-03 06:55:10 +0100

    Add else if

--- js/script.js
+++ js/script.js
@@ -10,3 +10,28 @@ function clearMessages(){
 var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+// losowanie liczby od 1-3
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+printMessage('Wylosowana liczba to: ' + randomNumber);
+
+// losowanie liczby od 11-19
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 9 + 10);
+printMessage('Wylosowana liczba to: ' + randomNumber);
+
+//losowanuie
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+} else if (randomNumber == '2') {
+	computerMove = 'papier';
+} else if (randomNumber == '3') {
+	computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
